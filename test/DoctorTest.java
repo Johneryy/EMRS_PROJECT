df5276ef2c6ce02c6061773a9f53ea276adee0ff
Johneryy <johnakintolu4@gmail.com>
2022-06-15 20:38:50 +0800

    emrs_project

---
+++ test/DoctorTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+class DoctorTest {
+
+  @Test
+    void getFirstName() {
+    }
+
+
+    void setFirstName() {
+    }
+
+
+    void getLastName() {
+    }
+
+
+    void setLastName() {
+    }
+
+    void getGender() {
+    }
+
+}
